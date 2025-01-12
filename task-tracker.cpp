7b340780fe82b8a0358942f250b1dbec7ea7ce1f
Sushan Shrestha <sushanstha523@gmail.com>
2025-01-12 21:12:41 +0545

    Implemented JSON format & some fixes

--- task-tracker.cpp
+++ task-tracker.cpp
@@ -11,12 +11,19 @@
 #include <iterator>
 
 // Bug: command arguments following "add" command works but are also added to the task-file as tasks.
+// Formed JSON object //
+// Form JSON array  //
+// Implement how to write the object/array to file in a proper format   //
+// Display tasks
+// You can do it!
+
+constexpr auto max_size = std::numeric_limits<std::streamsize>::max();
 
 // Function prototypes
 std::string argParse(std::string argv);
 
 class Task{
-    const std::string path = "C:\\Users\\Susha\\Documents\\Programming\\C++\\Projects\\Task Tracker\\tasks.json";
+    const std::string path = "tasks.json";
     int id;
     std::string description;
     std::string status =  "to-do";
@@ -25,14 +32,20 @@ class Task{
 
     // Private functions
     void createTaskFile(){
-        std::fstream file(path, std::ios::app);
+        std::ifstream ifile(path);
+        if(!ifile || fileEmpty()){
+            std::ofstream ofile(path, std::ios::out);
+            ofile << "[" << std::endl;
+            ofile << "]" << std::endl;
+            ofile.close();
+        }
         fileError();
-        file.close();
+        ifile.close();
     }
 
     // Error handling functions
     void fileError(){
-        std::fstream file(path);
+        std::ifstream file(path);
         if(!file){
             std::cerr << "Error: Couldn't access task file!" << std::endl;
             file.close();
@@ -41,7 +54,11 @@ class Task{
     }
 
     int fileEmpty(){
-        std::fstream file(path, std::ios::app);
+        std::ifstream file(path);
+        // std::vector<std::string> lines;
+        // readFile(lines);
+        // display(lines);
+        file.seekg(0);
         if(file.peek() == EOF){
             return 1;
         }
@@ -60,7 +77,90 @@ class Task{
         ifile.close();
     }
 
-    void writeFile(std::vector<std::string> lines){
+    void writeFile(std::vector<std::string>& lines){
+        // Read from file
+        std::string line;
+        std::vector<std::string> file_lines;
+        std::ifstream ifile(path);
+        readFile(file_lines);
+        ifile.close();
+
+        // Formats the std::ostringstream/JSON Object
+        std::ostringstream oss;
+        for(const auto& line: lines){
+            if(line == "[" || line == "]"){
+                oss << line << std::endl;
+            }
+            else if(line == "{" || line == "}"){
+                oss << std::setw(line.length()+2) << line << std::endl;
+            }
+            else{
+                oss << std::setw(line.length()+4) << line << std::endl;
+            }
+        }
+
+        // file_lines
+        std::vector<std::string> new_lines;
+        std::string temp;
+        for(auto it = file_lines.begin(); it != file_lines.end();){
+            if(*it == "]"){
+                temp = *it;
+                if(!file_lines.empty()){
+                    file_lines.pop_back();
+                }
+
+                std::stringstream ss;
+                if(ss << oss.str()){
+                    while(std::getline(ss, line)){
+                        new_lines.push_back(line);
+                    }
+                }
+                new_lines.push_back(temp);
+                break;
+            }
+
+    
+            // for(char x: *it){
+            //     if(x == '}' && *(it+1) != "]"){
+            //         std::cout << "x: " << std::endl;
+            //         *it = *it + ",";
+            //     }
+            // }
+
+            ++it;
+        }
+
+        // Merges new_lines into file_lines
+        file_lines.insert(file_lines.end(), new_lines.begin(), new_lines.end());
+        for(auto it = file_lines.begin(); it != file_lines.end(); it++){
+            for(char x: *it){
+                if(x == '}' && *it != "  }," && *(it+1) != "]"){
+                    *it = *it + ",";
+                }
+            }
+        }
+
+        // Write operation
+        std::ofstream ofile(path, std::ios::trunc | std::ios::out);
+        for(const auto& line: file_lines){
+            ofile << line << std::endl;
+        }
+        ofile.close();
+
+    }
+
+    void removeLastComma(std::vector<std::string>& lines){
+        for(auto it = lines.end()-2; it != lines.end(); it++){
+            if(*it == *(lines.end()-2) && *it == "  }," && *(it+1) == "]"){
+                std::string buff;
+                buff = *it;
+                buff.pop_back();
+                *it = buff;
+            }
+        }
+    }
+
+    void updateFile(std::vector<std::string>& lines){
         std::ofstream ofile(path, std::ios::out | std::ios::trunc);
         fileError();
         for(const auto& line: lines){
@@ -68,6 +168,30 @@ class Task{
         }
         ofile.close();
     }
+
+    void display(std::vector<std::string>& lines){
+        for(const auto& line: lines){
+            std::cout << line << std::endl;
+        }
+    }
+
+    // void writeFile(std::ostringstream& oss){
+    //     std::ofstream ofile(path, std::ios::app);
+    //     std::vector<std::string> lines;
+    //     readFile(lines);
+    //     std::vector<std::string>::iterator it;
+    //     for(const auto& line: lines){
+    //         if(line == "["){
+    //             ofile << line << std::endl;
+    //             ofile << oss.str() << "]" << std::endl;
+    //         }
+    //     }
+    //     // std::string last = *(lines.end()-1);
+    //     // for(it = lines.begin(); it != lines.end(); it++){
+
+    //     // }
+    //     ofile.close();
+    // }
 
     std::string date(){
         // std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
@@ -82,23 +206,19 @@ class Task{
         std::ifstream file(path, std::ios::in);
         fileError();
         std::string line;
+        id = 1;
         if(file.peek() != EOF){
             while(std::getline(file, line)){
                 std::stringstream ss(line);
-                std::string* str_buf = new std::string;
-                ss >> *str_buf;
-                if((*str_buf).compare("id:") == 0){
+                std::string str_buf;
+                ss >> str_buf;
+                if(str_buf == "\"id\":"){
                     int task_id;
                     ss >> task_id;
-                    if(ss){
+                    if(ss)
                         id = task_id + 1;
-                    }
                 }
-                delete str_buf;
             }
-        }
-        else {
-            id = 1;
         }
         file.close();
     }
@@ -125,40 +245,62 @@ public:
     // Member functions
     void addTask(std::string description){
         getTaskId();
-        std::ofstream file(path, std::ios::app);
-        file << "id: " << id << std::endl;
-        file << "description: " << description << std::endl;
-        file << "status: " << status << std::endl;
         updatedAt = createdAt = date();
-        file << "createdAt: "  << createdAt << std::endl;
-        file << "updatedAt: " << updatedAt << std::endl;
-        file << std::endl;
-        file.close();
+
+        std::vector<std::string> lines;
+        lines.push_back("{");
+        lines.push_back("\"id\": "+std::to_string(id));
+        lines.push_back("\"description\": \""+description+"\"");
+        lines.push_back("\"status\": \""+status+"\"");
+        lines.push_back("\"createdAt\": \""+createdAt+"\"");
+        lines.push_back("\"updatedAt\": \""+updatedAt+"\"");
+        lines.push_back("}");
+
+
+        // Adds comma to the end of line
+        for(auto it = lines.begin(); it != lines.end()-1; it++){
+            if(*it != "{" && *it != "["){
+                if(*(it+1) != "}" && *(it+1) != "]"){
+                    *it = *it + ",";
+                }
+            }
+        }
+
+        // File Write Operation
+        writeFile(lines);
+    
         std::clog << "Task added successfully! (ID: " << id << ")" << std::endl;
     }
 
     void updateTask(int task_id, std::string updated_description){
-        std::vector<std::string> lines;
-        readFile(lines);
-        if(!fileEmpty()){
+        if(fileEmpty()){
             std::cerr << "Error: Task file is empty!" << std::endl;
             return;
         }
-        
+
+        std::vector<std::string> lines;
+        readFile(lines);
+
         std::vector<std::string>::iterator it;
         for(it = lines.begin(); it < lines.end(); it++){
             std::stringstream ss(*it);
-            ss.ignore(4, ' ');
+            // ss.ignore(max_size, ' ');    // Didn't work
+            std::string buf;
+            ss >> buf;
             int n;
             if(ss >> n){
                 if(n == task_id){
-                    // const std::string desc = "description: "+updated_description;
-                    *(it+1) = "description: "+updated_description;
-                    writeFile(lines);
+                    std::string line = *(it+1);
+                    std::string space = std::string(4, ' ');
+                    *(it+1) = space + "\"description\": \"" + updated_description + "\",";
+                    *(it+4) = space + "\"updatedAt\": \"" + date() + "\"";
+                    updateFile(lines);
                     std::clog << "Task updated successfully!" << std::endl;
+                    return;
                 }
             }
         }
+        std::cerr << "Error: Task ID " << task_id << " not found!" << std::endl;
     }
 
     void deleteTask(int task_id){
@@ -173,31 +315,52 @@ public:
         for(it = lines.begin(); it != lines.end(); it++){
             std::stringstream ss(*it);
             
-            ss.ignore(4, ' ');
+            std::string buf;
+            ss >> buf;  // Unnecessary buffer
             int n;
-            if(!(ss >> n)){
-                continue;   // Ignore lines that don't contain a valid task ID
-            }
-            if(n == task_id){
-                int startLine;
-                const int numLinesToDelete = 6;
-                if(std::distance(lines.begin(), it) <= 0){
-                    startLine = 0;
-                }
-                else{
-                    startLine = std::distance(lines.begin(), it);
-                }
-                int endLine = std::min(startLine + numLinesToDelete, static_cast<int>(lines.size()));
+            if(ss >> n){
+                if(n == task_id){
+                    int startLine;
+                    const int numLinesToDelete = 7;
+                    if(std::distance(lines.begin(), it-1) <= 0){
+                        startLine = 0;
+                    }
+                    else{
+                        startLine = std::distance(lines.begin(), it-1);
+                    }
+                    int endLine = std::min(startLine + numLinesToDelete, static_cast<int>(lines.size()));
 
-                if(startLine >= 0 && startLine < lines.size() && startLine < endLine && endLine <= lines.size()){
-                    lines.erase(lines.begin() + startLine, lines.begin() + endLine);
-                    writeFile(lines);
-                    std::clog << "Task deleted successfully!" << std::endl;
-                    return;
-                }
-                else{
-                    std::cerr << "Error: Invalid range for erase operation!" << std::endl;
-                    return;
+                    if(startLine >= 0 && startLine < lines.size() && startLine < endLine && endLine <= lines.size()){
+                        lines.erase(lines.begin() + startLine, lines.begin() + endLine);
+
+                        // Lambda for ID Sorting
+                        auto taskIdSort = [&task_id, &it, &lines](){
+                            for(int i = 0; it != lines.end(); it++){
+                                std::stringstream ss(*it);
+                                std::string buff;
+                                ss >> buff;
+                                int n;
+                                if(ss >> n){
+                                    std::ostringstream oss;
+                                    buff = *it;
+                                    oss << std::setw(buff.length()-2) << "\"id\": " << task_id + i << ",";
+                                    *it = oss.str();
+                                    i++;
+                                }
+                            }
+                        };
+                        taskIdSort();
+                        removeLastComma(lines);
+                        
+                        // Updates File
+                        updateFile(lines);
+                        std::clog << "Task deleted successfully!" << std::endl;
+                        return;
+                    }
+                    else{
+                        std::cerr << "Error: Invalid range for erase operation!" << std::endl;
+                        return;
+                    }
                 }
             }
         }
@@ -221,14 +384,18 @@ public:
         std::vector<std::string>::iterator it;
         for(it = lines.begin(); it != lines.end(); it++){
             std::istringstream iss(*it);
-            iss.ignore(4);
+            // iss.ignore(4);   Didn't work
+            std::string temp;
+            iss >> temp;
             int no;
             if(iss >> no && no == task_id){
                 std::string stat[3] = {"to-do", "in-progress", "done"};
                 if(updated_status == stat[0] || updated_status == stat[1] || updated_status == stat[2]){
-                    *(it+2) = "status: " + updated_status;
-                    writeFile(lines);
-                    std::clog << "Status updated successfully!" << std::endl;
+                    std::string space = std::string(4, ' ');
+                    *(it+2) = space + "\"status\": \"" + updated_status + "\",";
+                    *(it+4) = space + "\"updatedAt\": \"" + date() + "\"";
+                    updateFile(lines);
+                    std::clog << "Task status updated successfully!" << std::endl;
                     return;
                 }
                 else{
@@ -251,10 +418,16 @@ public:
                 std::vector<std::string>::iterator it;
                 for(it = lines.begin(); it != lines.end(); it++){
                     std::istringstream iss(*it);
-                    iss.ignore(8);
+                    iss.ignore(15);
                     std::string c_status;
                     if(iss >> c_status){
+                        c_status.pop_back();
+                        c_status.pop_back();
+                        // std::cout << "status: " << c_status << std::endl;
+                        // std::cout << "t: " << task_status << std::endl;
                         if(c_status == task_status){
+                            // std::cout << "status: " << c_status << std::endl;
+                            // std::cout << "t: " << task_status << std::endl;
                             std::cout << *(it-2) << std::endl;
                             std::cout << *(it-1) << std::endl;
                             std::cout << *(it) << std::endl;
